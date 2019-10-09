bb214b887f8d63bcb53025250a10619a2ee3f6d7
Andrew Nikolajuk <42691598+anikolaj@users.noreply.github.com>
2019-10-09 15:09:07 -0500

    create service worker

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = "hello-pwa";
+var filesToCache = [
+  '/',
+  '/tones',
+  '/comments',
+  'app.js',
+  'manifest.json',
+  'views/index.ejs',
+  'views/tones.ejs',
+  'views/comments.ejs',
+  'css/styles.css',
+  'main.js'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil( caches.open(cacheName).then(function(cache) {
+    return cache.addAll(filesToCache);
+  }) );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(response) {
+    retuern response || fetch(e.request);
+  }) );
+});
